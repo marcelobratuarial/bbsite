d4d8be1f95468cbf81f06132ee1d074d831cc361
Andrew <yuutoandrew.jpn@gmail.com>
2021-10-08 09:50:34 -0300

    teste

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 // Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
+
 /*
  *---------------------------------------------------------------
  * BOOTSTRAP THE APPLICATION
