efe59ae3ed7266007b744ed973804f20b3199ee2
Marcelo Dênis <marcelo@agenciabrasildigital.com.br>
2021-10-13 15:02:41 -0300

    loading fix

--- app/Views/layouts/main/main.php
+++ app/Views/layouts/main/main.php
@@ -50,7 +50,7 @@
         <div class="CustomLoader active">
             <!-- <img src="<?= base_url('assets/images/loader/loading.png') ?>" alt=""> -->
             <video id="Loader" muted="muted" autoplay="autoplay">
-                <source src="<?= base_url('assets/videos/logoanimada02.mp4') ?>" type="video/mp4" />
+                <source src="<?= base_url('assets/videos/logoanimada03.mp4') ?>" type="video/mp4" />
             <!-- <source src="movie.ogg" type="video/ogg" /> -->
             Your browser does not support the video tag.
             </video>
