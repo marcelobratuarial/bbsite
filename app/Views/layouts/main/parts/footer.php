2a100cb22d2cfa46900ef0386d3115d949ffdbc3
Marcelo Dênis <marcelo@agenciabrasildigital.com.br>
2021-10-15 12:40:22 -0300

    footer adjustments

--- app/Views/layouts/main/parts/footer.php
+++ app/Views/layouts/main/parts/footer.php
@@ -1,70 +1,80 @@
 <section class="py-0" style="background-color: #3D4C6F;">
     <div class="container">
-        <div class="row align-items-center">
+        <!-- <div class="row">
+                <div class="col-6">
+                    <h1>Atendimento</h1>
+                    <label>Central de Relacionamento</label>
+                    <ion-icon name="logo-whatsapp"></ion-icon><span>(31) 2510-8536</span>
+                    <label>Central de Benefícios</label>
+                    <img src="img/whats.png" class="d-inline-block whats"><span>(31) 9 9279-0202</span>
+                    <label>Suporte Financeiro</label>
+                    <img src="img/whats.png" class="d-inline-block whats"><span>(31) 9 7337-0767</span>
+                    <label>Assistência 24 Horas</label>
+                    <span>0800 600 28 51</span>
+                </div>
+            </div> -->
+        <div class="row justify-content-between">
             <div class="col-lg-6">
                 <div class="p-5 p-lg-6 radius-secondary">
                    <img width="30%" src="<?= base_url("assets/images/landing/logo_branca_mini.svg")?>" alt="">
                    <h5 class="text-white mt-4">Conquistar um novo associado é muito bom, fidelizá-lo é muito melhor!</h5>
                 </div>
             </div>
-            <div class="col-lg-6 mt-4 mt-lg-0">
-                <div class="row">
-                    <div class="col-6 col-lg-4 color-white ml-lg-auto">
-                        <ul class="list-unstyled">
-                            <li class="mb-3">
-                                <a class="color-white" href="<?= base_url('contato') ?>">Fale Conosco</a>
-                            </li>
-                            <li class="mb-3">
-                                <a class="color-white" href="<?= base_url('faq') ?>">Perguntas Frequentes</a>
-                            </li>
-                            <li class="mb-3">
-                                <a class="color-white" href="<?= base_url('beneficios') ?>">Portal de Benefícios</a>
-                            </li>
-                            <li class="mb-3">
-                                <a class="color-white" href="#">Termos de Uso</a>
-                            </li>
-                            
-                        </ul>
+            
+            <div class="col-lg-4  mt-5 ">
+                <h4 class="color-white">Fale com a gente!</h4>
+                <a href="#">
+                    <div class="row mb-3 align-items-center no-gutters">
+                        <div class="col-auto">
+                            <div class="background-primary text-center d-flex align-items-center radius-primary" style="height: 40px; width: 40px;">
+                                <span class="w-100 fa fa-whatsapp color-white"></span>
+                            </div>
+                        </div>
+                        <div class="col-auto pl-3 color-white">
+                            <label>Central de Relacionamento</label><br>
+                            <span>(31) 2510-8536</span>
+                        </div>
                     </div>
-                    <div class="col-6 col-sm-5 ml-sm-auto">
-                        <a href="#">
-                            <div class="row mb-3 align-items-center no-gutters">
-                                <div class="col-auto">
-                                    <div class="background-primary text-center d-flex align-items-center radius-primary" style="height: 40px; width: 40px;">
-                                        <span class="w-100 fa fa-instagram color-white"></span>
-                                    </div>
-                                </div>
-                                <div class="col-6 pl-3">
-                                    <h5 class="fs-0 color-white mb-0 d-inline-block">Instagram</h5>
-                                </div>
+                </a>
+                <a href="#">
+                    <div class="row mb-3 align-items-center no-gutters">
+                        <div class="col-auto">
+                            <div class="background-primary text-center d-flex align-items-center radius-primary" style="height: 40px; width: 40px;">
+                                <span class="w-100 fa fa-whatsapp color-white"></span>
                             </div>
-                        </a>
-                        <a href="#">
-                            <div class="row mb-3 align-items-center no-gutters">
-                                <div class="col-auto">
-                                    <div class="background-primary text-center d-flex align-items-center radius-primary" style="height: 40px; width: 40px;">
-                                        <span class="w-100 fa fa-youtube color-white"></span>
-                                    </div>
-                                </div>
-                                <div class="col-6 pl-3">
-                                    <h5 class="fs-0 color-white mb-0 d-inline-block">Youtube</h5>
-                                </div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="row mb-3 align-items-center no-gutters">
-                                <div class="col-auto">
-                                    <div class="background-primary text-center d-flex align-items-center radius-primary" style="height: 40px; width: 40px;">
-                                        <span class="w-100 fa fa-facebook color-white"></span>
-                                    </div>
-                                </div>
-                                <div class="col-6 pl-3">
-                                    <h5 class="fs-0 color-white mb-0 d-inline-block">Facebook</h5>
-                                </div>
-                            </div>
-                        </a>
+                        </div>
+                        <div class="col-auto pl-3 color-white">
+                            <label>Central de Benefícios</label><br>
+                            <span>(31) 9 9279-0202</span>
+                        </div>
                     </div>
-                </div>
+                </a>
+                <a href="#">
+                    <div class="row mb-3 align-items-center no-gutters">
+                        <div class="col-auto">
+                            <div class="background-primary text-center d-flex align-items-center radius-primary" style="height: 40px; width: 40px;">
+                                <span class="w-100 fa fa-whatsapp color-white"></span>
+                            </div>
+                        </div>
+                        <div class="col-auto pl-3 color-white">
+                            <label>Suporte Financeiro</label><br>
+                            <span>(31) 9 7337-0767</span>
+                        </div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="row mb-3 align-items-center no-gutters">
+                        <div class="col-auto">
+                            <div class="background-primary text-center d-flex align-items-center radius-primary" style="height: 40px; width: 40px;">
+                                <span class="w-100 fa fa-headphones color-white"></span>
+                            </div>
+                        </div>
+                        <div class="col-auto pl-3 color-white">
+                        <label>Assistência 24 Horas</label><br>
+                        <span>0800 600 28 51</span>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
         <!--/.row-->
@@ -80,12 +90,42 @@
                     <img src="assets/images/logo-light.png" alt="" />
                 </a>
                 -->
+                <a class="color-white" href="<?= base_url('faq') ?>">Perguntas Frequentes</a>
             </div>
             <div class="col-sm-6 mt-3 mt-sm-0">
-                <p class="color-white lh-6 mb-0 fw-600">&copy; Copyright 2021</p>
+                <!-- <p class="color-white lh-6 mb-0 fw-600">&copy; Copyright 2021</p> -->
+                <span class="fs-1 color-white mr-3">Siga-nos: </span>
+                <a href="#">
+                    <span class="fs-1 mr-2 fa fa-facebook color-white"></span>
+                    
+                </a>
+                <a href="#">
+                    <span class="fs-1 mr-2 fa fa-youtube color-white"></span>
+                    
+                </a>
+                <a href="#">
+                    <span class="fs-1 mr-2 fa fa-instagram color-white"></span>
+                    
+                </a>
             </div>
             <div class="col-sm-3 text-sm-right mt-3 mt-sm-0 text-white">
-                <a class="color-white" href="<?= base_url("")?>">  Brasil Benefícios</a>
+                <!-- <ul class="list-unstyled">
+                    <li class="mb-3">
+                        <a class="color-white" href="<?= base_url('contato') ?>">Fale Conosco</a>
+                    </li>
+                    <li class="mb-3">
+                        
+                    </li>
+                    <li class="mb-3">
+                        <a class="color-white" href="<?= base_url('beneficios') ?>">Portal de Benefícios</a>
+                    </li>
+                    <li class="mb-3">
+                        <a class="color-white" href="#">Termos de Uso</a>
+                    </li>
+                    
+                </ul> -->
+                <a class="color-white" href="<?= base_url('beneficios') ?>">Portal de Benefícios</a>
+                <!-- <a class="color-white" href="<?= base_url("")?>">  Brasil Benefícios</a> -->
             </div>
         </div>
         <!--/.row-->
